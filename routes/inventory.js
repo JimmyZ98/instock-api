ab32f3d174e9c9ccfd6f5c6efa3de928daabeaff
Liz Meeker <lizmeeker90@gmail.com>
2022-06-15 18:25:49 -0400

    added inventory POST method

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,5 +3,29 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  const { itemName, description, category, status, quantity, warehouseID, warehouseName } = req.body;
+
+  if (!itemName || !description || !category || !status || !quantity || !warehouseID || !warehouseName) {
+    return res.status(400)
+      .send("Error! One or more of the required request fields are empty");
+  }
+  else {
+    const newItem = {
+      id: uuidv4(),
+      warehouseID,
+      warehouseName,
+      itemName,
+      description,
+      category,
+      status,
+      quantity
+    };
+    const oldInventoryList = JSON.parse(fs.readFileSync('data/inventories.json'));
+    let newInventoryList = [...oldInventoryList, newItem]; 
+    fs.writeFileSync('data/inventories.json', JSON.stringify(newInventoryList));
+    res.status(201).send(newItem.id);
+  };
+});
 
 module.exports = router;
